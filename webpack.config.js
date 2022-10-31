409c0c0b6e6df210d19c0a19fc626867184d1c87
Thomas <tho.daskalakis@gmail.com>
2022-10-31 08:45:01 +0200

    Fix project setup to use ts-loader

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,19 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
   devtool: 'inline-source-map',
   output: {
     filename: 'main.js',
